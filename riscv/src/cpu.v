67f3a82e3d4f41ec3c6b4f413cc544d003b28a03
zsq259 <2530059525@qq.com>
2024-01-07 19:42:34 +0800

    fix: makefile

--- riscv/src/cpu.v
+++ riscv/src/cpu.v
@@ -29,7 +29,7 @@ module cpu (
     // - 0x30004 read: read clocks passed since cpu starts (in dword, 4 bytes)
     // - 0x30004 write: indicates program stop (will output '\0' through uart tx)    
 
-    wire reddy_in = rdy_in & !io_buffer_full;
+    wire reddy_in = rdy_in && !io_buffer_full;
     
     wire i_m_ready;
 
